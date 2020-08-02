2319842ac01eb6484c5653b1b9b3f3e187fcb618
Prashubh Atri <prashubh27@gmail.com>
2020-08-02 21:53:28 +0530

    main.cpp by PrashubhAtri

---
+++ PrashubhAtri_COE/main.cpp
@@ -0,0 +1,35 @@
+/*
+    Code by Prashubh Atri
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f first
+#define s second
+#define int long long int
+#define pb push_back
+#define mp make_pair
+#define setbits(x) __builtin_popcount(x)
+#define mod 1000000007
+#define inf 1e18
+
+
+void ioControl(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+
+int32_t main(){
+    // ioControl();
+
+    // int test;cin>>test;while(test--){}
+
+    cout<<"Hello World";
+
+    return 0;
+}
